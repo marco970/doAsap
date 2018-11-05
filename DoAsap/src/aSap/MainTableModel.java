7d19059137686129332dfe40b1f005034e898349
marco970 <m.kuciak@gmail.com>
2018-11-05 08:41:37 +0100

    uruchomienie, pamiętać o ścieżce pliku

--- DoAsap/src/aSap/MainTableModel.java
+++ DoAsap/src/aSap/MainTableModel.java
@@ -28,7 +28,9 @@ public class MainTableModel extends AbstractTableModel {
 	
 	
 	//private String current = "F:/aSapData/Current3.txt";
-	private String current = "D:/Documents/00_projekty/doAsapVer2/aSapData/Current3.txt";
+	//private String current = "D:/Documents/00_projekty/doAsapVer2/aSapData/Current3.txt";
+	private String current = "C:/Users/marcin.kuciak/Documents/workIT_projects/doAsap/aSapData/Current3.txt";
+	
 	private Object[][] dane = null;
 	
 	public MainTableModel() 	{
